ff0ee4f610fb5cc78dd5bb852d62c7da8f1a2bcc
NicHub <nicolas.jeanmonod@gmail.com>
2026-02-08 23:04:28 +0100

    ajout du js pour target_blank auto + ajout des billets manquants

---
+++ scripts.js
@@ -0,0 +1,32 @@
+
+function targetBlank() {
+    const _a = document.getElementsByTagName("a");
+    const _siteHost = location.host.replace(/^www\./i, "");
+    const internalRegex = new RegExp(_siteHost, "i");
+
+    for (let i = 0; i < _a.length; i++) {
+        let href = _a[i].href;
+        let isExternal = false;
+
+        if (/^mailto:/i.test(href)) {
+            // Si le lien commence par mailto: il est forcément externe
+            isExternal = true;
+        } else if (location.protocol === "file:") {
+            // Si on est en protocole file://
+            // tous les liens http/https et protocol-relative sont externes
+            isExternal = /^(https?:)?\/\//i.test(href);
+        } else {
+            // Logique normale pour http/https
+            // Un lien est externe s’il a un host et que ce host ne correspond pas au site actuel
+            let linkHost = _a[i].host;
+            isExternal = linkHost && !internalRegex.test(linkHost);
+        }
+
+        if (isExternal) {
+            _a[i].setAttribute("target", "_blank");
+            _a[i].setAttribute("rel", "noopener");
+        }
+        // console.log(`${String(isExternal).padEnd(5)} ${_siteHost} ${href}`);
+    }
+}
+targetBlank();
